09f0b586fb7ed4bd98123f639ba8d8219b895008
patry <patryk.g@reperaturki.pl>
2024-11-17 17:39:22 +0100

    fix: InPost parcel lockers

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,7 +8,16 @@ export default defineNuxtConfig({
       meta: [
         {
           "http-equiv": "Content-Security-Policy",
-          content: `default-src * 'unsafe-inline' 'unsafe-eval'; script-src * 'unsafe-inline' 'unsafe-eval'; connect-src * 'unsafe-inline'; img-src * data: blob: 'unsafe-inline'; frame-src *; style-src * 'unsafe-inline';
+          content: `default-src * data: mediastream: blob: filesystem: about: ws: wss: 'unsafe-eval' 'wasm-unsafe-eval' 'unsafe-inline'; 
+script-src * data: blob: 'unsafe-inline' 'unsafe-eval'; 
+script-src-elem * data: blob: 'unsafe-inline' 'unsafe-eval';
+connect-src * data: blob: 'unsafe-inline'; 
+img-src * data: blob: 'unsafe-inline'; 
+media-src * data: blob: 'unsafe-inline'; 
+frame-src * data: blob: ; 
+style-src * data: blob: 'unsafe-inline';
+font-src * data: blob: 'unsafe-inline';
+frame-ancestors * data: blob:;
           `,
         },
       ],
