da9ea5fd13cb0b7165b38604e45fea8ddc0741ca
patry <patryk.g@reperaturki.pl>
2024-11-18 21:15:55 +0100

    feat: Add listing orders and changing account data

--- pages/konto/index.vue
+++ pages/konto/index.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
 import { ROUTES } from "../../constants/routes";
 
+const loading = ref<boolean>(false);
+
 const nuxtApp = useNuxtApp();
 
 const sessionStore = useSessionStore();
+
+const snackbarStore = useSnackbarStore();
 
 const medusaClient = nuxtApp.$medusaClient;
 
@@ -12,6 +16,66 @@ const router = useRouter();
 const { width, height } = useWindowSize();
 
 const showOrders = ref<boolean>(false);
+
+const email = ref<string>(sessionStore.session?.email!);
+const firstName = ref<string>(sessionStore.session?.first_name!);
+const lastName = ref<string>(sessionStore.session?.last_name!);
+
+const config = useRuntimeConfig();
+
+watch(showOrders, async (newValue) => {
+  if (newValue && !ordersList.value) {
+    loading.value = true;
+    await loadOrders();
+    loading.value = false;
+  }
+});
+
+const ordersList = ref<any[]>();
+
+const loadOrders = async () => {
+  try {
+    const { orders } = await medusaClient.store.order.list();
+
+    ordersList.value = orders;
+
+    console.log("order response", orders);
+  } catch (e) {
+    snackbarStore.showSnackbar(
+      "Wystąpił problem podczas pobierania zamówień",
+      "error",
+      5000
+    );
+  }
+};
+
+const saveAccountData = async () => {
+  try {
+    loading.value = true;
+
+    if (firstName.value === "" || lastName.value === "") {
+      snackbarStore.showSnackbar("Wartości nie mogą być puste", "error", 5000);
+      loading.value = false;
+      return;
+    }
+
+    await medusaClient.store.customer.update({
+      first_name: firstName.value,
+      last_name: lastName.value,
+    });
+
+    snackbarStore.showSnackbar("Zmieniono dane konta", "success", 3000);
+    loading.value = false;
+    showChangeAccountDataDialog.value = false;
+  } catch (e) {
+    snackbarStore.showSnackbar("Wystąpił błąd", "error", 5000);
+    loading.value = false;
+  }
+};
+
+const showChangeAccountDataDialog = ref<boolean>(false);
+
+loadOrders();
 
 const logout = async () => {
   try {
@@ -60,8 +124,33 @@ const logout = async () => {
         <br />
         <p>
           Dane lub hasło uległy zmianie?
-          <v-btn variant="text" color="primary">Zmień dane</v-btn>
+          <v-btn
+            variant="text"
+            @click="showChangeAccountDataDialog = true"
+            color="primary"
+            >Zmień dane</v-btn
+          >
         </p>
+        <v-dialog :width="width * 0.5" v-model="showChangeAccountDataDialog">
+          <v-card>
+            <v-card-title> Zmiana danych konta </v-card-title>
+            <v-card-text>
+              <v-text-field
+                v-model="email"
+                label="Email"
+                disabled
+              ></v-text-field>
+              <v-text-field v-model="firstName" label="Imię"></v-text-field>
+              <v-text-field v-model="lastName" label="Nazwisko"></v-text-field>
+            </v-card-text>
+            <v-card-actions>
+              <v-btn @click="showChangeAccountDataDialog = false">Anuluj</v-btn>
+              <v-btn @click="saveAccountData" :loading="loading" color="primary"
+                >Zapisz</v-btn
+              >
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
         <br />
         <v-btn color="primary" @click="logout">Wyloguj się</v-btn>
       </v-card-text>
@@ -70,17 +159,39 @@ const logout = async () => {
       <v-card-title>Twoje zamówienia</v-card-title>
       <v-card-subtitle>Tutaj możesz zobaczyć swoje zamówienia</v-card-subtitle>
       <v-card-text>
-        Lista zamówień będzie Tutaj
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
+        <v-progress-circular indeterminate v-if="loading"></v-progress-circular>
+        <p v-if="!loading && !ordersList">Brak zamówień do wyświetlenia</p>
+        <v-list v-if="!loading && ordersList">
+          <v-list-item v-for="order in ordersList" :key="order.id">
+            <v-list-item-title
+              >Zamówienie numer: {{ order.display_id }}</v-list-item-title
+            >
+            <v-list-item-subtitle
+              >Złożone dnia: {{ new Date(order.created_at).getDate() }}/{{
+                new Date(order.created_at).getMonth()
+              }}/{{ new Date(order.created_at).getFullYear() }}
+            </v-list-item-subtitle>
+            <br />
+            <p>
+              Kwota:
+              {{
+                new Intl.NumberFormat("pl-PL", {
+                  style: "currency",
+                  currency: "PLN",
+                }).format(order.total)
+              }}
+            </p>
+            <br />
+            <v-list-item-action>
+              <v-btn
+                color="primary"
+                :to="`/potwierdzenie-zamowienia/${order.id}`"
+                >Zobacz zamówienie</v-btn
+              >
+            </v-list-item-action>
+            <br />
+          </v-list-item>
+        </v-list>
       </v-card-text>
     </v-card>
   </v-sheet>
