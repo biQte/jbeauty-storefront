d6e1ac5cf75f1506e4930ea6cae62811c8d39472
patry <patryk.g@reperaturki.pl>
2024-12-31 00:52:52 +0100

    fix: Check if parcel locker fix works

--- pages/realizacja-zamowienia.vue
+++ pages/realizacja-zamowienia.vue
@@ -1085,7 +1085,7 @@ onMounted(() => {
                 language="pl"
                 style="width: 100%; height: 500px; overflow: hidden"
                 onpoint="onpointselect"
-                config="parcellocker247"
+                config="parcelcollect247"
               ></inpost-geowidget>
 
               <!-- </div> -->
