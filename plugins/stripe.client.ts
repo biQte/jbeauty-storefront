ef748ee884801f74511eb8f44d4c83cfe78257c8
patry <patryk.g@reperaturki.pl>
2024-12-31 03:43:35 +0100

    fix: Try to fix orders with stripe not processing sometimes

--- plugins/stripe.client.ts
+++ plugins/stripe.client.ts
@@ -2,6 +2,7 @@ import { loadStripe } from "@stripe/stripe-js";
 
 export default defineNuxtPlugin(async () => {
   const stripe = await loadStripe(String(process.env.NUXT_PUBLIC_STRIPE_KEY!));
+  console.log(process.env.NUXT_PUBLIC_STRIPE_KEY);
   return {
     provide: {
       stripe,
