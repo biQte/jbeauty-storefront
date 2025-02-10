c97fb0837d0d47d5490942e3eeab7eb246716f39
patry <patryk.g@reperaturki.pl>
2025-02-10 14:31:32 +0100

    feat: Update home page banner

--- components/TheHomePageLowerBanner.vue
+++ components/TheHomePageLowerBanner.vue
@@ -7,10 +7,13 @@ const config = useRuntimeConfig();
     <!-- <v-img src="@/assets/img/fotos-352.jpg" /> -->
     <!-- <v-img src="@/assets/img/fotos-363.jpg" /> -->
     <!-- <NuxtLink class="left banner" :to="`/kategoria/swiateczne-perelki`"> -->
-    <NuxtLink class="left banner" :to="`/kategoria/victoria-vynn-brillant-gel`">
+    <NuxtLink
+      class="left banner"
+      :to="`/produkt/excellent-pro-magic-shine-35ml`"
+    >
       <div class="overlay">
         <!-- <p>Świąteczne perełki</p> -->
-        <p>Brilliant Gel</p>
+        <p>Magic Shine</p>
         <v-btn :elevation="24" size="x-large" color="transparent"
           >Zobacz więcej</v-btn
         >
@@ -76,7 +79,8 @@ const config = useRuntimeConfig();
   .left {
     // background-image: url("/public/product_gallery_3f99f2894a.jpg");
     // background-image: url("/public/VV_PinkProsecco_1200x1200_PLE-dystry.jpg");
-    background-image: url("/public/VYNN_VV_BRILLANT_1200_PLE.webp");
+    // background-image: url("/public/VYNN_VV_BRILLANT_1200_PLE.webp");
+    background-image: url("/public/ExcellentPRO-Magic-Shine-35-ml-baner.webp");
     background-size: cover;
   }
   .right {
