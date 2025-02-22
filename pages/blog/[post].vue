27079c05f49740cafb92222f9763269dcb2c2483
patry <patryk.g@reperaturki.pl>
2025-02-22 23:43:28 +0100

    feat: Upload blog view

---
+++ pages/blog/[post].vue
@@ -0,0 +1,151 @@
+<!-- <script setup lang="ts">
+const route = useRoute();
+const { data: post } = await useAsyncData("post", () =>
+  queryCollection("blog")
+    .where("draft", "<>", false)
+    .where("title", "=", `${route.params.post}`)
+    .first()
+);
+
+const { width, height } = useWindowSize();
+</script>
+
+<template>
+  <v-sheet class="blog-post-wrapper">
+    <v-card v-if="post" :width="width > 720 ? width * 0.7 : width * 0.9">
+      <h1>{{ post.title }}</h1>
+    </v-card>
+    <v-card v-else> Nie znaleziono wpisu </v-card>
+  </v-sheet>
+</template>
+
+<style lang="scss" scoped>
+.blog-post-wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  min-height: 90vh;
+  gap: 1rem;
+
+  @media only screen and (max-width: 720px) {
+    align-items: center;
+    justify-content: flex-start;
+  }
+}
+</style> -->
+
+<script setup lang="ts">
+const route = useRoute();
+const { data: post } = await useAsyncData("post", () =>
+  queryCollection("blog")
+    .where("draft", "<>", false)
+    .where("title", "=", `${route.params.post}`)
+    .first()
+);
+
+useSeoMeta({
+  title: post.value?.title + " - JBeauty",
+  ogTitle: post.value?.title + " - JBeauty",
+  ogImage: post.value?.thumbnail,
+  keywords: post.value?.tags.join(","),
+});
+</script>
+
+<template>
+  <v-container class="blog-post-wrapper">
+    <v-card v-if="post" class="blog-post-card">
+      <v-img :src="post.thumbnail" contain class="blog-post-image" />
+      <!-- lazy-src="/placeholder.jpg" -->
+      <v-card-title class="blog-post-title"
+        ><h1>{{ post.title }}</h1></v-card-title
+      >
+      <v-card-subtitle class="blog-post-date">
+        {{ new Date(post.date).toLocaleDateString("pl-PL") }}
+      </v-card-subtitle>
+
+      <v-card-text class="blog-post-content">
+        <ContentRenderer :value="post.body" />
+      </v-card-text>
+    </v-card>
+
+    <v-card v-else class="not-found">Nie znaleziono wpisu</v-card>
+  </v-container>
+</template>
+
+<style lang="scss" scoped>
+.blog-post-wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  min-height: 90vh;
+  padding: 2rem;
+}
+
+.blog-post-card {
+  max-width: 1080px;
+  width: 100%;
+  padding: 1rem;
+
+  @media only screen and (max-width: 720px) {
+    padding: 0;
+  }
+}
+
+.blog-post-image {
+  height: auto;
+  // max-height: 350px;
+  border-radius: 8px;
+}
+
+.blog-post-title {
+  font-size: 2rem;
+  font-weight: 700;
+  margin-top: 1rem;
+
+  ::v-deep(h1) {
+    all: revert;
+    font-size: 2rem;
+    font-weight: 400;
+  }
+}
+
+.blog-post-date {
+  font-size: 1rem;
+  color: gray;
+}
+
+.blog-post-content {
+  font-size: 1.1rem;
+  line-height: 1.6;
+  padding-top: 1rem;
+
+  div > * {
+    margin-top: 1rem;
+  }
+
+  div {
+    ::v-deep(h1),
+    ::v-deep(h2),
+    ::v-deep(h3),
+    ::v-deep(h4),
+    ::v-deep(h5),
+    ::v-deep(h6) {
+      all: revert;
+
+      a {
+        color: black;
+        text-decoration: none;
+      }
+    }
+
+    ::v-deep(a) {
+      all: revert;
+      color: $primary-color;
+      text-decoration: none;
+    }
+  }
+}
+</style>
---
+++ pages/blog/index.vue
@@ -0,0 +1,101 @@
+<script setup lang="ts">
+const { data: posts } = await useAsyncData("blog", () =>
+  queryCollection("blog")
+    .where("draft", "<>", false)
+    .order("date", "DESC")
+    .all()
+);
+
+const { width } = useWindowSize();
+
+useSeoMeta({
+  title: "Blog - JBeauty",
+  ogTitle: "Blog - JBeauty",
+});
+</script>
+
+<template>
+  <v-container class="blog-wrapper">
+    <h1 class="blog-title">Blog</h1>
+    <v-row justify="center">
+      <v-col v-for="post in posts" :key="post.id" cols="12">
+        <v-card class="blog-card" :to="`/blog/${post.title}`">
+          <v-img
+            :src="post.thumbnail"
+            cover
+            class="blog-image"
+            :alt="`Okładka postu ${post.title}`"
+            max-height="350px"
+          />
+          <v-card-title class="blog-card-title">
+            {{ post.title }}
+          </v-card-title>
+          <v-card-subtitle class="blog-card-date">
+            {{ new Date(post.date).toLocaleDateString("pl-PL") }}
+          </v-card-subtitle>
+          <v-card-text class="blog-card-text">
+            {{ post.description.slice(0, 250) + "..." }}
+          </v-card-text>
+          <v-card-actions>
+            <v-btn color="primary" variant="text">Czytaj więcej</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<style lang="scss" scoped>
+.blog-wrapper {
+  width: 100%;
+  min-height: 90vh;
+  padding: 2rem;
+  max-width: 900px;
+  margin: auto;
+}
+
+.blog-title {
+  text-align: center;
+  font-size: 2.5rem;
+  font-weight: 600;
+  margin-bottom: 2rem;
+}
+
+.blog-card {
+  display: flex;
+  flex-direction: column;
+  overflow: hidden;
+  border-radius: 12px;
+  transition: transform 0.3s ease-in-out, box-shadow 0.3s ease-in-out;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
+
+  &:hover {
+    transform: translateY(-5px);
+    box-shadow: 0px 8px 20px rgba(0, 0, 0, 0.2);
+  }
+}
+
+.blog-image {
+  width: 100%;
+  height: auto;
+  object-fit: cover;
+}
+
+.blog-card-title {
+  font-size: 1.8rem;
+  font-weight: 600;
+  padding: 1rem;
+}
+
+.blog-card-date {
+  font-size: 1rem;
+  color: gray;
+  padding: 0 1rem;
+}
+
+.blog-card-text {
+  padding: 0 1rem 1rem;
+  font-size: 1rem;
+  color: #444;
+}
+</style>
