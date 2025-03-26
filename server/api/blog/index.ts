9c0433aa46543a9579684cc6b7aa0c611793de10
biQte <patrykgonet@gmail.com>
2025-03-26 10:22:54 +0100

    Fix: Load blog posts on server to propely load blog page from direct link

--- server/api/blog/index.ts
+++ server/api/blog/index.ts
@@ -1,6 +1,6 @@
 export default eventHandler(async (event) => {
   const posts = await queryCollection(event, "blog")
-    .where("draft", "<>", "false")
+    .where("draft", "<>", false)
     .order("date", "DESC")
     .all();
   return posts;
