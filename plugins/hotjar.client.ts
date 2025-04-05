cfe9c0026a59b237ecdd086f22889f2c08cbc567
biQte <patrykgonet@gmail.com>
2025-04-05 14:41:13 +0200

    FEAT: Add hotjar

--- plugins/hotjar.client.ts
+++ plugins/hotjar.client.ts
@@ -1,19 +1,23 @@
 export default defineNuxtPlugin(() => {
   if (process.env.NODE_ENV !== "production") return;
-  (function (h: any, o: any, t: any, j: any, a?: any, r?: any) {
-    h.hj =
-      h.hj ||
+  //@ts-expect-error
+  (function (c: any, s: any, q: any, u: any, a: any, r: any, e: any) {
+    c.hj =
+      c.hj ||
       function () {
-        (h.hj.q = h.hj.q || []).push(arguments);
+        (c.hj.q = c.hj.q || []).push(arguments);
       };
-    h._hjSettings = {
-      hjid: process.env.NUXT_HOTJAR_ID, // <-- tutaj wstaw swój ID z Hotjara
-      hjsv: 6,
-    };
-    a = o.getElementsByTagName("head")[0];
-    r = o.createElement("script");
-    r.async = true;
-    r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
-    a.appendChild(r);
-  })(window, document, "https://static.hotjar.com/c/hotjar-", ".js?sv=");
+    c._hjSettings = { hjid: a };
+    r = s.getElementsByTagName("head")[0];
+    e = s.createElement("script");
+    e.async = true;
+    e.src = q + c._hjSettings.hjid + u;
+    r.appendChild(e);
+  })(
+    window,
+    document,
+    "https://static.hj.contentsquare.net/c/csq-",
+    ".js",
+    process.env.NUXT_HOTJAR_ID
+  );
 });
