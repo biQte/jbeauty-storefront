91822d9abe154a6ee53d8e824a588cc4d1f0522f
biQte <patrykgonet@gmail.com>
2025-05-15 20:44:22 +0200

    FEAT: Add custom component for main page carousel

---
+++ components/TheMainPageCarouselItem.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+const props = defineProps<{
+  desktop: string;
+  mobile: string;
+  alt: string;
+  isFirst?: boolean;
+}>();
+
+const {width} = useWindowSize({initialWidth: 390});
+const isMobile = computed(() => width.value < 768);
+
+const loading = computed(() => props.isFirst ? 'eager' : 'lazy');
+const importance = computed(() => props.isFirst ? 'high' : 'low');
+</script>
+
+<template>
+    <nuxt-img
+    :src="isMobile ? mobile : desktop"
+    :loading="loading"
+    :importance="importance"
+    :alt="alt"
+    format="webp"
+    quality="80"
+    class="w-full h-full object-cover"
+    :sizes="[320, 640, 768, 1024, 1280, 2000]"
+    fit="contain"
+  />
+</template>
