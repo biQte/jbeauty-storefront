b10de1bbd958b8faa2dcf973cf725e288ada9b49
biQte <patrykgonet@gmail.com>
2025-05-31 14:56:42 +0200

    feat: add components for new account pages

---
+++ components/Account/Links.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+const route = useRoute();
+const router = useRouter();
+
+const path = computed(() => {
+    return route.path;
+});
+
+console.log('route', route);
+
+console.log('path', path.value);
+</script>
+
+<template>
+    <div class="account-page-toolbar">
+      <v-toolbar
+      density="compact"
+
+      color="transparent"
+    >
+      <v-btn
+        active-color="primary"
+        rounded="false"
+        :active="path === '/konto'"
+        @click="router.push('/konto')"
+      >
+        Dane
+      </v-btn>
+      <v-btn
+        active-color="primary"
+        rounded="false"
+        :active="path === '/konto/zamowienia'"
+        @click="router.push('/konto/zamowienia')"
+      >
+        Zamówienia
+      </v-btn>
+      <v-btn
+        active-color="primary"
+        rounded="false"
+        :active="path === '/konto/adresy'"
+        @click="router.push('/konto/adresy')"
+      >
+        Adresy
+      </v-btn>
+    </v-toolbar>
+    </div>
+</template>
+
+<style scoped lang="scss">
+.account-page-toolbar{
+  // width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
