f580effb93144898d80a5e3e52e718fd13edf8d0
biQte <patrykgonet@gmail.com>
2025-06-12 00:10:43 +0200

    feat: load hotjar a little bit later

--- plugins/hotjar.client.ts
+++ plugins/hotjar.client.ts
@@ -1,23 +1,27 @@
 export default defineNuxtPlugin(() => {
-  if (process.env.NODE_ENV !== "production") return;
-  //@ts-expect-error
-  (function (c: any, s: any, q: any, u: any, a: any, r: any, e: any) {
-    c.hj =
-      c.hj ||
-      function () {
-        (c.hj.q = c.hj.q || []).push(arguments);
-      };
-    c._hjSettings = { hjid: a };
-    r = s.getElementsByTagName("head")[0];
-    e = s.createElement("script");
-    e.async = true;
-    e.src = q + c._hjSettings.hjid + u;
-    r.appendChild(e);
-  })(
-    window,
-    document,
-    `https://static.hj.contentsquare.net/c/csq-`,
-    ".js",
-    5362100
-  );
+  if(import.meta.client){
+    setTimeout(() => {
+      if (process.env.NODE_ENV !== "production") return;
+      //@ts-expect-error
+      (function (c: any, s: any, q: any, u: any, a: any, r: any, e: any) {
+        c.hj =
+          c.hj ||
+          function () {
+            (c.hj.q = c.hj.q || []).push(arguments);
+          };
+        c._hjSettings = { hjid: a };
+        r = s.getElementsByTagName("head")[0];
+        e = s.createElement("script");
+        e.async = true;
+        e.src = q + c._hjSettings.hjid + u;
+        r.appendChild(e);
+      })(
+        window,
+        document,
+        `https://static.hj.contentsquare.net/c/csq-`,
+        ".js",
+        5362100
+      );
+    });
+  }
 });
